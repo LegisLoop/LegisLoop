fe6cb25ec5ffabd401209f6c93f89760ddb4b297
Jonathan Memoli <jonomemoli3@gmail.com>
2025-03-12 22:34:05 -0400

    Make frontend connection slightly better, interacts with the sidebar now

--- legisloop-frontend/src/pages/FindYourRepresentativePage.js
+++ legisloop-frontend/src/pages/FindYourRepresentativePage.js
@@ -7,19 +7,21 @@ import axios from 'axios';
 
 function FindYourRepresentativesPage() {
     const [representatives, setRepresentatives] = useState([]);
+    const [stateId, setStateId] = useState();
+
     useEffect(() => {
         const fetchRepresentatives = async () => {
             try {
-                const response = await axios.get("http://localhost:8080/api/v1/representative/stateId/30");
+                const response = await axios.get(`http://localhost:8080/api/v1/representative/stateId/${stateId}`);
                 console.log('response', response.data);
-                setRepresentatives(response.data.slice(0, 20)); // just get the first 10 for now 
+                setRepresentatives(response.data.slice(0, 20)); // just get the first 20 for now 
             } catch (error) {
                 console.error("Error fetching representatives:", error);
             }
         };
 
         fetchRepresentatives();
-    }, []);
+    }, [stateId]);
 
     return (
         <div className="flex flex-col">
@@ -29,7 +31,7 @@ function FindYourRepresentativesPage() {
             <div className="flex flex-grow">
                 {/* Sidebar (fixed width) */}
                 <div className="w-1/4 min-w-[250px] bg-gray-100">
-                    <RepresentativeSideBar />
+                    <RepresentativeSideBar setStateId={setStateId} />
                 </div>
 
                 {/* Representative Grid (takes remaining space) */}
