c474698a18adfb2e8226a7e872d935ef4ee1ff95
Jonathan Memoli <jonomemoli3@gmail.com>
2025-04-03 16:32:20 -0400

    Readd state mapping array

--- legisloop-frontend/src/customHooks/useGeoLocation.js
+++ legisloop-frontend/src/customHooks/useGeoLocation.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
-import { stateMapping } from "../constants/statemap";
+import stateMapping from "../constants/statemap";
 
 function useGeoLocation(shouldRequest) {
     const [stateId, setStateId] = useState(null);
