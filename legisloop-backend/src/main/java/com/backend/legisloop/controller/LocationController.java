465d4e5ce72391bcb377f1090e46aa7c6b422fba
Jonathan Memoli <jonomemoli3@gmail.com>
2025-04-03 14:20:31 -0400

    Move location api call to the backend

---
+++ legisloop-backend/src/main/java/com/backend/legisloop/controller/LocationController.java
@@ -0,0 +1,22 @@
+package com.backend.legisloop.controller;
+
+import com.backend.legisloop.enums.StateEnum;
+import com.backend.legisloop.service.LocationService;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/location")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
+public class LocationController {
+
+    private final LocationService locationService;
+
+    @GetMapping("/state")
+    public ResponseEntity<StateEnum> getState(@RequestParam double lat, @RequestParam double lon) throws UnirestException {
+        return ResponseEntity.ok(locationService.getUserLocation(lat, lon));
+    }
+}
